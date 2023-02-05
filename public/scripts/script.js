f2e7a38074cfb85516d96d0f268769a0d23d8342
Lightnindrummer <dersu13@yahoo.com>
2023-02-05 02:15:23 -0500

    commit new changes 	modified:   public/index.html 	modified:   public/scripts/script.js 	modified:   public/style.css

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -21,20 +21,21 @@ var loginButtons = document.getElementsByClassName("LoginContainer")[0].children
 handleEvents(loginButtons[0], "click", displayLandingPageForms);
 handleEvents(loginButtons[1].children[0], "click", displayLandingPageForms);
 function displayLandingPageForms() {
-  var loginForm = document.getElementsByClassName("login-form")[0]; 
-  var signupForm = document.getElementsByClassName("signup-form")[0]
+  var loginForm = document.getElementsByClassName("login-form")[0];
+  var signupForm = document.getElementsByClassName("signup-form")[0];
   console.log(loginButtons);
   loginButtons[0].style.display = "none";
-  loginButtons[1].style.display = "none";
+  loginButtons[1].style.display = "none"; 
+  loginButtons[0].parentElement.style.borderRadius = "50px"
   if (this == loginButtons[0]) {
     loginForm.style.display = "block";
+    setTimeout(() => {
+      loginForm.style.opacity = 1;
+    }, 100);
+  } else if (this == loginButtons[1].children[0]) {
+    signupForm.style.display = "block"; 
     setTimeout(() => { 
-      loginForm.style.animation = "fadein 1.2s ease-in" 
-    }, 500);
-  } else if (this == loginButtons[1].children[0]) { 
-    signupForm.style.display = "block";
-    setTimeout(() => { 
-      signupForm.style.animation = "fadein 1.2s ease-in" 
-    }, 500);
+      signupForm.style.opacity = 1;
+    }, 100);
   }
 }
